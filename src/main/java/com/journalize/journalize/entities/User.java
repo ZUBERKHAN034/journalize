35482599b2cdb26de8e00595b20f39cb22ae222e
Zuber Khan <zuberkhan034@gmail.com>
2026-06-02 14:56:44 +0530

    Update .gitignore

---
+++ src/main/java/com/journalize/journalize/entities/User.java
@@ -0,0 +1,30 @@
+package com.journalize.journalize.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Document(collection = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    private String id;
+
+    @Indexed(unique = true)
+    @NonNull
+    private String username;
+
+    @NonNull
+    private String password;
+}
